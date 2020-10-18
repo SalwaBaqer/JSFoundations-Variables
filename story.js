b9f7230752a0a7beea377bc5a29aaf0afe780951
salwa a <SalwaBaqer>
2020-10-18 20:01:37 +0300

    New Story

---
+++ story.js
@@ -0,0 +1,8 @@
+let Name = "Salwa";
+let Age= "25";
+let Hobby= "Chilling";
+
+
+// Mr./Mrs. Mshary, in 33 years, you will be 58 years old. By then you'll have decades of Meditation practice!
+
+console.log(`Ms. ${Name}, in 33 years, you will be ${Age + 33} years old. By then you'll have decades of ${Hobby} practice!`);
